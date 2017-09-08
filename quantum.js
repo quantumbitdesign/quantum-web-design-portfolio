deb905434891a6f07fe3d01d6a79bb1de06eaa86
Kevin Goudswaard <kevingoudswaard@gmail.com>
2017-09-08 00:10:37 -0700

    Huge Update!
    More Styling, added a video the the main landing page, resolved footer, header, navigation, and main content scaling issues. Added responsiveness for two breakpoints, and updated content.

--- quantum.js
+++ quantum.js
@@ -2,24 +2,44 @@
 
 
 let nav = document.querySelector("#mainSideNav");
+let allContent = document.querySelector(".pageWrapper");
+let allIndexHTMLContent = document.querySelector(".homePageWrapper");
 let overlay = document.querySelector("#overlay");
 
 
 // Navigation Styling
 
 const openNav = () => {
-    nav.style.width = "250px";
+    nav.style.width = "300px";
+	allContent.style.marginLeft = "300px";
 	overlay.style.display = "block";
 }
 
 const closeNav = () => {
     nav.style.width = "0";
+	allContent.style.marginLeft = "auto";
+	overlay.style.display = "none";
+}
+
+const indexOpenNav = () => {
+    nav.style.width = "300px";
+	allIndexHTMLContent.style.marginLeft = "300px";
+	overlay.style.display = "block";
+}
+
+const indexCloseNav = () => {
+    nav.style.width = "0";
+	allIndexHTMLContent.style.marginLeft = "auto";
 	overlay.style.display = "none";
 }
 
 
+// Video check for available data
 
-
+let vid = document.querySelector("#backgroundVideo");
+vid.addEventListener( "canplay", function() {
+    vid.play();
+  });
 
 
 
