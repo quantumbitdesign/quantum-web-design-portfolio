0ee2d965b8e478c6287835a8de89fd153e18bf6f
Kevin Goudswaard <kevingoudswaard@gmail.com>
2017-09-09 15:46:06 -0700

    Finishing Touches Added
    The final product is here! Just updated everything basically. Looking really good, though it can always improve in dozens of ways. I have not optimized it either for frontend performance. to be continued.. but for now, its time to celebrate!

--- quantum.js
+++ quantum.js
@@ -1,6 +1,6 @@
 //variables
 
-
+let vid = document.querySelector("#backgroundVideo");
 let nav = document.querySelector("#mainSideNav");
 let allContent = document.querySelector(".pageWrapper");
 let allIndexHTMLContent = document.querySelector(".homePageWrapper");
@@ -8,6 +8,11 @@ let overlay = document.querySelector("#overlay");
 let rightTabContent = document.querySelector(".rightTabContent");
 let rightTab = document.querySelector(".rightTab");
 let footer = document.querySelector("footer");
+let list = document.querySelector("#list");
+let newListItemValue = document.querySelector("#listTextField");
+const e = document.querySelector("#selector");
+let playerChoice = document.querySelector(".playerChoice");
+let computerChoice = document.querySelector(".computerChoice");
 
 
 
@@ -99,14 +104,129 @@ const closeTab = () => {
 
 
 
+//                  //           List App JS
+
+
+
+const addNewEntry = () => {
+	let listItem = newListItemValue.value;
+	let newEntry = document.createElement("li");
+	newEntry.textContent = listItem;
+	list.appendChild(newEntry);
+	document.querySelector("#listTextField").value = "";
+}
+
+const removeNewEntry = () => {
+	let lastEntry = list.lastChild;
+	list.removeChild(lastEntry);
+}
+
+ const nuke = () => {
+	list.innerHTML = "";
+}
+
+
+
+
+
+
+
+
+//                  //              Rock, Paper, Scissors JS
+
+const playerWin = () => {
+	playerChoice.style.backgroundColor = "green";
+	computerChoice.style.backgroundColor = "red";
+}
+
+const playerLose = () => {
+	playerChoice.style.backgroundColor = "red";
+	computerChoice.style.backgroundColor = "green";
+}
+
+const tie = () => {
+	playerChoice.style.backgroundColor = "green";
+	computerChoice.style.backgroundColor = "green";
+}
+
+
+const battleSim = () => {
+  let playerChoice = e.options[e.selectedIndex].value;
+  let messageText = document.querySelector("#resultMessage");
+  let compChoice = Math.floor(Math.random() * (4 - 1) + 1);
+  
+  
+  if (playerChoice === "rock") {
+		document.querySelector("#playerPic").src = "images/rock.jpg";
+	} else if (playerChoice === "paper") {
+		document.querySelector("#playerPic").src = "images/paper.jpg";
+	} else {
+		document.querySelector("#playerPic").src = "images/scissors.jpg";
+	}
+  
+  
+    if (compChoice === 1 && playerChoice === "rock") {
+	  document.querySelector("#computerPic").src = "images/rock.jpg";
+      resultMessage.innerHTML = "It's a tie!";
+	  tie();
+	  
+	  
+    } else if (compChoice === 2 && playerChoice === "rock") {
+	  document.querySelector("#computerPic").src = "images/paper.jpg";
+      resultMessage.innerHTML = "computer wins, paper covers rock";
+	  playerLose();
+	  
+	  
+    } else if (compChoice === 3 && playerChoice === "rock") {
+	  document.querySelector("#computerPic").src = "images/scissors.jpg";
+      resultMessage.innerHTML = "you win, rock destroys scissors!";
+	  playerWin();
+	  
+    } else if (compChoice === 1 && playerChoice === "scissors") {
+	  document.querySelector("#computerPic").src = "images/rock.jpg";
+      resultMessage.innerHTML = "computer wins, rock destroys scissors!";
+	  playerLose();
+	  
+    } else if (compChoice === 2 && playerChoice === "scissors") {
+	  document.querySelector("#computerPic").src = "images/paper.jpg";
+      resultMessage.innerHTML = "you win, scissors cut paper!";
+	  playerWin();
+	  
+    } else if (compChoice === 3 && playerChoice === "scissors") {
+	  document.querySelector("#computerPic").src = "images/scissors.jpg";
+      resultMessage.innerHTML = "It's a tie!";
+	  tie();
+	  
+    } else if (compChoice === 1 && playerChoice === "paper") {
+	  document.querySelector("#computerPic").src = "images/rock.jpg";
+      resultMessage.innerHTML = "you win, paper covers rock!";
+	  playerWin();
+	  
+    } else if (compChoice === 2 && playerChoice === "paper") {
+	  document.querySelector("#computerPic").src = "images/paper.jpg";
+      resultMessage.innerHTML = "It's a tie!";
+	  tie();
+	  
+    } else {
+	  document.querySelector("#computerPic").src = "images/scissors.jpg";
+      resultMessage.innerHTML = "computer wins, scissors cut paper!";
+	  playerLose();
+    }
+}
+
+
+
+
+
+
 //                  //              Video check for available data
 
 
-let vid = document.querySelector("#backgroundVideo");
-vid.addEventListener( "canplay", function() {
-    vid.play();
+const vidLoad = () => {
+		vid.addEventListener( "canplay", function() {
+		vid.play();
   });
-
+}
 
 
 
